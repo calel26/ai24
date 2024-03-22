2bbffe8a88907efcb962a8d6ab6edda13e15186f
Lawson <lawson@lawsmat.com>
2024-03-21 22:56:43 -0400

    countries calculations

--- python/ml/countries.py
+++ python/ml/countries.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 df = pd.read_csv("./countries.csv")
 print(df.shape)
@@ -10,29 +9,17 @@ print(df.shape)
 # Step 1: Filter the DataFrame for the countries of interest
 us = df[df.country == "United States"]
 jp = df[df.country == "Japan"]
-
-# calculate increase
+us['pct'] = us['population'].pct_change() * 100
+jp['pct'] = jp['population'].pct_change() * 100
 
 plt.figure(figsize=(10, 6))  # Set the figure size for better readability
-plt.plot(us['year'], us['population'], label=us['country'])
-plt.plot(jp['year'], jp['population'], label=jp['country'])
 
 plt.xlabel('Year')  # X-axis label
 plt.ylabel('Population')  # Y-axis label
-plt.title('Population Growth')  # Chart title
+
+plt.plot(us['year'], us['pct'], label=us['country'])
+plt.plot(jp['year'], jp['pct'], label=jp['country'])
+
+plt.title('Population Increase (%)')  # Chart title
 # plt.legend()  # Show legend to identify the lines
 plt.show()  # Display the plot
-'''   
-# Plotting
-plt.figure(figsize=(10, 6))  # Set the figure size for better readability
-
-for country in ['China', 'United States', 'Japan']:
-    country_df = filtered_df[filtered_df['country'] == country]
-    plt.plot(country_df['year'], country_df['population'], label=country)
-
-plt.xlabel('Year')  # X-axis label
-plt.ylabel('Population')  # Y-axis label
-plt.title('Population Growth')  # Chart title
-plt.legend()  # Show legend to identify the lines
-plt.show()  # Display the plot
-'''
